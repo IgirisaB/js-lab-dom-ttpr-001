82bcc3e34b310052cfb0bb7f5c10f32743d611d2
Brian Igirisa <brian.igirisa@gmail.com>
2025-05-01 16:54:55 -0400

    Commint to add saving theme

--- js/main.js
+++ js/main.js
@@ -8,9 +8,28 @@ function toggleDarkMode() {
     
   */
     document.body.classList.toggle("dark");
+
+  // Extra credit: save theme to local storage
+  // Save the current theme to Local Storage
+  const currentTheme = document.body.classList.contains("dark") ? "dark" : "light";
+  localStorage.setItem("theme", currentTheme);
     
 }
 
 document
   .getElementById("toggle-btn")
   .addEventListener("click", toggleDarkMode);
+
+  
+
+// a function to set the theme that's saved on page load
+
+function setThemeOnLoad() {
+  const savedTheme = localStorage.getItem("theme");
+  if (savedTheme === "dark") {
+    document.body.classList.add("dark");
+  }
+}
+
+// Call setThemeOnLoad when the page loads
+setThemeOnLoad();
